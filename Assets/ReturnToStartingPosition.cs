9555b75657b5d6c7d6e3fddb6f1ce38f6db6eb27
Shahan Akhter <shahan3.14@gmail.com>
2015-07-08 04:33:24 -0400

    started testing the reverse shatter effect

---
+++ Assets/ReturnToStartingPosition.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections;
+
+public class ReturnToStartingPosition : MonoBehaviour
+{
+
+    Vector3 startPosition, driftPosition;
+    Quaternion startRotation, driftRotation;
+
+    public float driftSeconds = 3;
+    private float driftTimer = 0;
+
+    public bool isDrifting;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+
+    public void StartDrift()
+    {
+        isDrifting = true;
+        driftTimer = 0;
+        driftPosition = transform.position;
+        driftRotation = transform.rotation;
+
+        // Make sure has rigidbody
+        rb.velocity = Vector3.zero;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    public void StopDrift()
+    {
+        isDrifting = false;
+        driftTimer = 0;
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+
+         // Make sure has rigidbody
+        rb.velocity = Vector3.zero;
+        rb.constraints = RigidbodyConstraints.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            StartDrift();
+
+        if (isDrifting)
+        {
+            driftTimer += Time.deltaTime;
+
+            if (driftTimer > driftSeconds)
+            {
+                StopDrift();
+            }
+            else
+            {
+                float ratio = driftTimer / driftSeconds;
+                transform.position = Vector3.Lerp(driftPosition, startPosition, ratio);
+                transform.rotation = Quaternion.Slerp(driftRotation, startRotation, ratio);
+            }
+        }
+
+    }
+
+}
