2cb03844c0867078e7f6bd7306a50fa0689cdd28
Shahan Akhter <shahan3.14@gmail.com>
2015-07-09 00:06:07 -0400

    minor change to unshatter script

--- Assets/ReturnToStartingPosition.cs
+++ Assets/ReturnToStartingPosition.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ReturnToStartingPosition : MonoBehaviour
 {
 
+    // Maybe require component RigidBody
     Vector3 startPosition, driftPosition;
     Quaternion startRotation, driftRotation;
 
@@ -22,7 +23,6 @@ public class ReturnToStartingPosition : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
         startPosition = transform.position;
         startRotation = transform.rotation;
     }
@@ -46,7 +46,7 @@ public class ReturnToStartingPosition : MonoBehaviour
         transform.position = startPosition;
         transform.rotation = startRotation;
 
-         // Make sure has rigidbody
+        // Make sure has rigidbody
         rb.velocity = Vector3.zero;
         rb.constraints = RigidbodyConstraints.None;
     }
@@ -72,7 +72,6 @@ public class ReturnToStartingPosition : MonoBehaviour
                 transform.rotation = Quaternion.Slerp(driftRotation, startRotation, ratio);
             }
         }
-
     }
 
 }
